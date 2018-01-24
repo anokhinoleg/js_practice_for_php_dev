2fd737c42630b18c9348137531331ee00e20c941
Oleg Anokhin <olegyurievich1991@gmail.com>
2018-01-24 08:49:32 +0200

    minor change for errorData

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -52,7 +52,8 @@
                         resolve(data)
                     });
                 }).catch(function (jqXHR) {
-                    reject(jqXHR);
+                    let errorData = JSON.parse(jqXHR.responseText);
+                    reject(errorData);
                 });
             });
         },
@@ -110,8 +111,7 @@
             .then(function(data) {
                 self._clearForm();
                 self._addRow(data);
-            }).catch(function (jqXHR) {
-                let errorData = JSON.parse(jqXHR.responseText);
+            }).catch(function (errorData) {
                 self._mapErrorsToForm(errorData.errors);
             });
         },
