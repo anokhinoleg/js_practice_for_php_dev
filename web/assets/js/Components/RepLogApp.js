bfd377d4fa848f41bf650c0796dd841087e2111e
Oleg Anokhin <olegyurievich1991@gmail.com>
2018-03-07 08:49:59 +0200

    require css via webpack

--- web/assets/js/Components/RepLogApp.js
+++ web/assets/js/Components/RepLogApp.js
@@ -2,6 +2,7 @@
 const Helper = require('./RepLogAppHelper');
 const $ = require('jquery');
 const swal = require('sweetalert2');
+require('sweetalert2/dist/sweetalert2.css');
 
 let HelperInstance = new WeakMap();
 class RepLogApp {
--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,18 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
+                exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader"
+                    loader: "babel-loader",
+                    options: {
+                        cacheDirectory: true
+                    }
                 }
+            },
+
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
             }
         ]
     },
