1331ee6daae1981ed455ec9637490a13579e7c2f
Anokhin Oleg <olegyurievich1991@gmail.com>
2018-07-14 18:41:17 +0300

    partial require

--- assets/js/Components/RepLogApp.js
+++ assets/js/Components/RepLogApp.js
@@ -3,6 +3,8 @@ const Helper = require('./RepLogAppHelper');
 const $ = require('jquery');
 const swal = require('sweetalert2');
 require('sweetalert2/dist/sweetalert2.css');
+const Routing = require('./Routing');
+const _ = require('lodash/number');
 
 let HelperInstance = new WeakMap();
 class RepLogApp {
@@ -14,7 +16,7 @@ class RepLogApp {
         for (let repLog of initialRepLogs) {
             this._addRow(repLog);
         }
-
+        this._clearForm();
         this.$wrapper.on(
             'click',
             '.js-delete-rep-log',
@@ -143,6 +145,7 @@ class RepLogApp {
         this._removeFormErrors();
         const $form = this.$wrapper.find(RepLogApp._selectors.newRepForm);
         $form[0].reset();
+        $form.find('[name="reps"]').val(_.random(1, 10));
     }
 
     _removeFormErrors() {
